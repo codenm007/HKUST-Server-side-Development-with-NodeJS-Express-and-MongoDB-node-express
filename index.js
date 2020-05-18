4ba34a6ee48b1c4206015954693646ad97f84a7e
NILANJAN MAJUMDAR <n.majumder14@gmail.com>
2020-05-19 00:14:58 +0530

    Added assignment 1 solution

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const http = require("http");
 const morgan = require("morgan");
 const  bodyParser = require("body-parser");
 
+//importing routes
 const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
 
 const hostname = "localhost";
 const port = 3000;
@@ -12,8 +15,14 @@ const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-
+//using route files
 app.use("/dishes",dishRouter);
+
+app.use("/promotions",promoRouter);
+
+app.use("/leaders",leaderRouter);
+
+
 app.use(express.static(__dirname + "/public" ));
 
 app.use((req,res,next)=>{
